aeacbec3c1da42ec7da257dddecbf9f3d1a65bc7
AKINOLA AYOBAMI <ayobamiakinola84@gmail.com>
2025-04-24 10:33:36 +0100

    feat: database, post

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
