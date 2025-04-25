619e506dc63eb86b8807ba730f3e50b27ff72452
AKINOLA AYOBAMI <ayobamiakinola84@gmail.com>
2025-04-25 13:04:09 +0100

    feat: catch all

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsOptions } from './config/corsOptions';
+import { AllExceptionsFilter } from './allexceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.setGlobalPrefix('api');
   app.enableCors(corsOptions);
   await app.listen(process.env.PORT ?? 3000);
